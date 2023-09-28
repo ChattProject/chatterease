526dc0467dd320733ba8178e8d4ef8f9571e04a2
Bielkina <belkina.olena.m@gmail.com>
2023-09-28 23:28:35 +0300

    fixed bug + add page Personal message

--- src/pages/PersonalMessage/PersonalMessage.jsx
+++ src/pages/PersonalMessage/PersonalMessage.jsx
@@ -1,12 +1,26 @@
-export const PersonalMessage = ({}) => {
+import { Link } from "react-router-dom";
+
+import './PersonalMessage.scss';
+export const PersonalMessage = ({ }) => {
   return (
     <>
-      <div className="personal">
+      <div className="chatpage__links">
+          <div className="chatpage__chats chatpage__subtitle chatpage__subtitle_selected paragraph">
+            <Link className="chatpage__link">Чати</Link>
+          </div>
+          <div className="chatpage__personal chatpage__subtitle paragraph">
+            <Link to={"/direct"} className="chatpage__link">
+              Особисті повідомлення
+            </Link>
+          </div>
+        </div>
+      
+      <div className="personal chat">
         <div className="personal__window">
           <div className="personal__title">Ця сторінка скоро з’явиться</div>
           <div className="personal__text">
             Наші розробники уже на фінішній прямій і скоро ми додамо можливість
-            особистого спілкуванняту:
+            особистого спілкування
           </div>
         </div>
       </div>
