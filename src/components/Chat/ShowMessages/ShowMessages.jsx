19bc8f373d78891b7b0e17a3a77750349f21eaee
Hanna Kuzii <hanna.kuzii@gmail.com>
2023-10-09 09:57:14 +0300

    Fixed bugs arrow and scroll

--- src/components/Chat/ShowMessages/ShowMessages.jsx
+++ src/components/Chat/ShowMessages/ShowMessages.jsx
@@ -1,29 +1,72 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import up from "../../../images/chat/up.svg";
 
 export const ShowMessages = ({ messages, message, userName }) => {
   const containerRef = useRef(null);
   const messagesRef = useRef(null);
+  const [showButtonUp, setShowButtonUp] = useState(false);
+  const [autoScroll, setAutoScroll] = useState(true);
 
   const goToTop = () => {
     containerRef.current.scrollIntoView({
-      // top: 0,
       behavior: "smooth",
       block: "start",
       inline: "nearest",
     });
+    setShowButtonUp(false);
+  };
+
+  const goToBottom = () => {
+    containerRef.current.scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+      inline: "nearest",
+    });
+    setShowButtonUp(true);
   };
 
   useEffect(() => {
-    if (containerRef.current) {
+    if (containerRef.current && autoScroll) {
       containerRef.current.scrollIntoView({
         behavior: "smooth",
         block: "end",
         inline: "nearest",
       });
+      setShowButtonUp(true);
     }
-  }, [messages]);
+  }, [messages, autoScroll]);
 
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     if (containerRef.current) {
+  //       const scrollTop = containerRef.current.scrollTop;
+  //       const scrollHeight = containerRef.current.scrollHeight;
+  //       const clientHeight = containerRef.current.clientHeight;
+        
+  //       // Розрахунок 5% від відстані від верху контейнера до верху прокрученого вмісту
+  //       const fivePercent = (scrollHeight - clientHeight) * 0.05;
+  
+  //       setShowButtonUp(scrollTop > fivePercent);
+        
+  //       if (scrollTop + clientHeight >= scrollHeight - fivePercent) {
+  //         setAutoScroll(true); // Автоматична прокрутка, коли користувач на дні
+  //       } else {
+  //         setAutoScroll(false); // Вимкнути автоматичну прокрутку, коли користувач прокручує вгору
+  //       }
+  //     }
+  //   };
+  
+  //   if (containerRef.current) {
+  //     containerRef.current.addEventListener("scroll", handleScroll);
+  //   }
+  
+  //   return () => {
+  //     if (containerRef.current) {
+  //       containerRef.current.removeEventListener("scroll", handleScroll);
+  //     }
+  //   };
+  // }, []);
+  
   function getDate(posted) {
     const date = new Date(posted);
 
@@ -63,9 +106,17 @@ export const ShowMessages = ({ messages, message, userName }) => {
   }
   return (
     <>
-      <div className="show_message container">
-        <div className={"show_message__messages"} ref={containerRef}>
-          <button className="show_message__up" onClick={goToTop}>
+      <div className={`show_message container ${userName && 'show_message_input'}`}>
+        <div className={'show_message__messages'} ref={containerRef}>
+          <button
+            className={`show_message__button ${
+              showButtonUp
+                ? "show_message__button_up"
+                : "show_message__button_down"
+            }
+            ${userName ? "show_message__button_input" : ""}`}
+            onClick={showButtonUp ? goToTop : goToBottom}
+          >
             <img src={up} alt="up" className="show_message__up_icon" />
           </button>
           {messages?.map((card) => {
