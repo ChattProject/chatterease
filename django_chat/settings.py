ec76fcc59f6cb472476da8b30ebc11f9f95d2906
bezkrovnyyav <bezkrovnyyav@gmail.com>
2023-10-28 18:31:01 +0300

    fixed settings

--- django_chat/settings.py
+++ django_chat/settings.py
@@ -29,7 +29,7 @@ load_dotenv(dotenv_path=ENV_PATH)
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-v%@et)in#0asq2_24cst6*)jp%829hyg1vqn&b5d-+#@nwx2dp'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -115,17 +115,17 @@ REST_FRAMEWORK = {
 
 
 
-DATABASES = {
-	"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
-
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
+# 	"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 ## User model
 AUTH_USER_MODEL = 'members.Members'
