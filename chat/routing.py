ba28a97893a59058012876f9586a8bcb5bd8d9cd
bezkrovnyyav <bezkrovnyyav@gmail.com>
2023-10-28 19:56:33 +0300

    changed wss conections

--- chat/routing.py
+++ chat/routing.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import consumers
 
 websocket_urlpatterns = [
-	path(r"ws/open_chat/<uuid>/", consumers.JoinAndLeave.as_asgi())
+	path(r"wss/open_chat/<uuid>/", consumers.JoinAndLeave.as_asgi())
 ]
