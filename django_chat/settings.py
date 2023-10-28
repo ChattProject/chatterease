c37bf8e8550385f3ec28acd0197dc1c9e6afd28a
bezkrovnyyav <bezkrovnyyav@gmail.com>
2023-10-28 18:50:35 +0300

    fixed settings

--- django_chat/settings.py
+++ django_chat/settings.py
@@ -115,17 +115,17 @@ REST_FRAMEWORK = {
 
 
 
-# DATABASES = {
-# 	"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
-# }
-
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	"default": dj_database_url.parse("sqlite:///db.sqlite3")
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 ## User model
 AUTH_USER_MODEL = 'members.Members'
