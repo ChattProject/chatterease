5ec0741cce0a752b3663e6201d1a47a0c757a564
Bielkina <belkina.olena.m@gmail.com>
2023-10-30 07:58:17 +0200

    close modal window from  support

--- src/components/Home/GreetingDots/GreetingDots.jsx
+++ src/components/Home/GreetingDots/GreetingDots.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 
 export const GreetingDots = ({ choosedPage, handlePageClick }) => {
-    console.log(choosedPage)
-    return (
-        
-        <div className="dots">
-            <ul className="dots__list">
-            {[0, 1, 2].map((item) => (
-                <li key={item}>
-                <button
-                    className={`dots__item ${item === choosedPage ? 'active' : ''}`}
-                    onClick={() => handlePageClick(item)}
-                ></button>
-                </li>
-            ))}
-            </ul>
-        </div>   
-    )
-    
-}
+  return (
+    <div className="dots">
+      <ul className="dots__list">
+        {[0, 1, 2].map((item) => (
+          <li key={item}>
+            <button
+              className={`dots__item ${item <= choosedPage ? 'active' : ''}`}
+              onClick={() => handlePageClick(item)}
+            ></button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
