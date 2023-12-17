f7f4c46e4e4428e855a3278afe7d0b7f26e5081b
Bielkina <belkina.olena.m@gmail.com>
2023-12-17 11:53:39 +0200

    last massege

--- src/components/Chat/ShowMessages/ShowMessages.jsx
+++ src/components/Chat/ShowMessages/ShowMessages.jsx
@@ -1,25 +1,31 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import up from "../../../images/chat/up.svg";
-
 export const ShowMessages = ({
   messages,
   message,
   userName,
   containerRef,
-  scrollToBottom,
   searchInChat,
   setSearchNothingVisible
 }) => {
-  // const containerRef = useRef(null);
   const [showButtonUp, setShowButtonUp] = useState(false);
-
+  const lastMessageRef = useRef(null);
+const scrollToLastMessage = () => {
+  if (lastMessageRef.current) {
+    lastMessageRef.current.scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+      inline: "nearest",
+    });
+  }
+};
   useEffect(() => {
     const hasSearchInChat = messages.some((message) =>
       message.content.includes(searchInChat)
     );
     setSearchNothingVisible(!hasSearchInChat);
-  }, [messages, searchInChat]);
-
+    scrollToLastMessage();
+  }, [messages, searchInChat, setSearchNothingVisible]);
   const goToTop = () => {
     containerRef.current.scrollIntoView({
       behavior: "smooth",
@@ -28,21 +34,13 @@ export const ShowMessages = ({
     });
     setShowButtonUp(false);
   };
-
   const goToBottom = () => {
-    containerRef.current.scrollIntoView({
-      behavior: "smooth",
-      block: "end",
-      inline: "nearest",
-    });
+    scrollToLastMessage();
     setShowButtonUp(true);
   };
-
   function getDate(posted) {
     const date = new Date(posted);
-
     const year = date.getFullYear();
-
     function fullNumber(number) {
       if (number.toString().length === 1) {
         return `0${number}`;
@@ -50,17 +48,13 @@ export const ShowMessages = ({
         return number;
       }
     }
-
     const day = fullNumber(date.getDate());
     const month = fullNumber(date.getMonth() + 1);
-
     const hours = fullNumber(date.getHours());
     const minutes = fullNumber(date.getMinutes());
     const seconds = fullNumber(date.getSeconds());
-
     return `${hours}:${minutes}:${seconds} ${day}/${`${month}`}/${year}`;
   }
-
   return (
     <>
       <div className={`show_message ${userName && "show_message_input"}`}>
@@ -77,36 +71,39 @@ export const ShowMessages = ({
         </button>
         <div className={"show_message__messages"} ref={containerRef}>
           {messages?.map((card, index) => {
-            const words = card.content.split(" ");
-
-            return (
-              <div
-                className={`show_message__message  message ${
-                  userName === card.author && "message_right"
-                } ${card.id === messages.length - 1 && "message__last"}`}
-                key={card.id + index}
-              >
-                <div className="message__name">{card.author}</div>
-                <div className="message__text">
-                  {words.map((word, index) => (
-                    <span
-                      key={index + word.length}
-                      className={
-                        searchInChat !== "" && word.includes(searchInChat)
-                          ? "message__text_bold"
-                          : ""
-                      }
-                    >
-                      {`${word} `}
-                    </span>
-                  ))}
+            if (card && card.content) {
+              const words = card.content.split(" ");
+              const isLastMessage = index === messages.length - 1;
+              return (
+                <div
+                  className={`show_message__message  message ${
+                    userName === card.author && "message_right"
+                  } ${isLastMessage && "message__last"}`}
+                  key={card.id + index}
+                  ref={isLastMessage ? lastMessageRef : null}
+                >
+                  <div className="message__name">{card.author}</div>
+                  <div className="message__text">
+                    {words.map((word, index) => (
+                      <span
+                        key={index + word.length}
+                        className={
+                          searchInChat !== "" && word.includes(searchInChat)
+                            ? "message__text_bold"
+                            : ""
+                        }
+                      >
+                        {`${word} `}
+                      </span>
+                    ))}
+                  </div>
+                  <div className="message__date">{getDate(card.timestamp)}</div>
                 </div>
-                <div className="message__date">{getDate(card.timestamp)}</div>
-              </div>
-            );
+              );
+            }
+            return null;
           })}
         </div>
-        {/* {message && <div>{userName} is typing...</div>} */}
       </div>
     </>
   );
