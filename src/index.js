04577aee129021b248390d82365da6fcf3e1e61a
Hanna Kuzii <hanna.kuzii@gmail.com>
2024-01-10 21:53:25 +0200

    fixed routing

--- src/index.js
+++ src/index.js
@@ -6,18 +6,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
-import { BrowserRouter, HashRouter } from "react-router-dom";
-import { createBrowserHistory } from 'history';
-
-const history = createBrowserHistory();
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter history={history} basename="/chatterease">
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
